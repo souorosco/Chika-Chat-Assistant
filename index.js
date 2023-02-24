09bd9ea3bf59f2de2a51c0d3a474861bdb2e86bf
Pedro Henrique Orosco de Camargo <pedrohoroscoc@hotmail.com.br>
2023-02-24 08:11:52 -0300

    Mudei coisa pra caralho.

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const { Client, MessageMedia, LocalAuth } = require("whatsapp-web.js")
-const qrcode = require("qrcode-terminal")
-const axios = require("axios")
-const ImageService = require("./utils/ImageService.js")
+console.log("O Chika Chat: Started.");
 
-const imageService = new ImageService()
+const { Client, MessageMedia, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const ImageService = require("./utils/ImageService.js");
+
+console.log("O Chika Chat: Import OK.");
+
+const imageService = new ImageService();
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     ffmpegPath: '/usr/bin/ffmpeg',
     puppeteer: {
-        executablePath: '/usr/bin/google-chrome-stable'
+        executablePath: '/usr/bin/google-chrome-stable',
+        args: ['--no-sandbox', "--disabled-setupid-sandbox"]
     }
 });
 
+console.log("O Chika Chat: Instances OK.");
+
 client.on("qr", qr => {
-    qrcode.generate(qr, { small: true })
+    qrcode.generate(qr, { small: true });
 });
 
+console.log("O Chika Chat: QR Code OK.");
+
 client.on("ready", () => {
-    console.log("O Chika Chat está pronta!")
+    console.log("O Chika Chat is ready!");
 });
 
 client.on("message_create", msg => {
     const command = msg.body.split(" ")[0];
-    const sender = msg.from.includes("5517996529815") ? msg.to : msg.from //5517996529815 //5517997122611
-    if (command === "/sticker" || command === "/8ball") msg.reply("Por favor, utilize .comando no lugar de /comando")
-    if (command === ".help") help(msg, sender)
-    if (command === ".sticker") generateSticker(msg, sender)
-    if (command === ".magic") magic(msg, sender)
-    if (command === ".8ball") eightBallMsg(msg)
-    if (command === ".roll") rollDice(msg)
+    const sender = msg.from.includes("5517996529815") ? msg.to : msg.from; //5517996529815 //5517997122611
+    if (command === ".help") help(msg, sender);
+    if (command === ".sticker") generateSticker(msg, sender);
+    if (command === ".magic") magic(msg, sender);
+    if (command === ".8ball") eightBallMsg(msg);
+    if (command === ".roll") rollDice(msg);
 });
 
 client.initialize();
@@ -37,65 +44,65 @@ client.initialize();
 const help = async (msg, sender) => {
     await client.sendMessage(sender, `Os comandos disponíveis são: 🤖
 
-    🖼️
-    • [foto] .sticker -> Transforma uma imagem enviada em sticker! ( *NOVO:* agora funciona com gif's)
-    • [foto] .sticker texto -> Transforma uma imagem enviada em sticker com o texto personalizado!
-    • [foto] .magic -> Transforma uma imagem enviada em sticker distorcido!
-    • [foto] .magic texto -> Transforma uma imagem enviada em sticker distorcido com o texto personalizado!
-    • .sticker [link] -> Transforma a imagem do link em sticker! (não faz stickers animados)
+🖼️
+• [foto] .sticker -> Transforma uma imagem enviada em sticker!
+• [foto] .sticker texto -> Transforma uma imagem enviada em sticker com o texto personalizado!
+• [foto] .magic -> Transforma uma imagem enviada em sticker distorcido!
+• [foto] .magic texto -> Transforma uma imagem enviada em sticker distorcido com o texto personalizado!
+• .sticker [link] -> Transforma a imagem do link em sticker! (não faz stickers animados)
 
-    🎱
-    • .8ball [pergunta] -> Responde uma pergunta de sim ou não. Descubra sua sorte!
-    
-    🎲
-    • .roll [número] -> Roda um dado do número de faces escolhido. (Em branco ou inválido rodará um D6)
-    
-    💭
-    Mais comandos em breve!`)
-}
+🎱
+• .8ball [pergunta] -> Responde uma pergunta de sim ou não. Descubra sua sorte!
+
+🎲
+• .roll [número] -> Roda um dado do número de faces escolhido. (Em branco ou inválido rodará um D6)
+
+💭
+Mais comandos em breve!`);
+};
 
 const magic = async (msg, sender) => {
     try {
-        const customMessage = msg.body.split('.magic')[1]
-        const { pathResponse } = await imageService.downloadImage(msg)
-        const { path, size } = await imageService.imageManipulation(pathResponse, customMessage.length > 0 ? customMessage : '', true)
-        const inBase64 = await imageService.trasnformImageTo64(path)
-        const response = new MessageMedia()
-        response.mimetype = "image/png"
-        response.data = inBase64
-        response.filesize = size
-        await client.sendMessage(sender, response, { sendMediaAsSticker: true })
+        const customMessage = msg.body.split('.magic')[1];
+        const { pathResponse } = await imageService.downloadImage(msg);
+        const { path, size } = await imageService.imageManipulation(pathResponse, customMessage.length > 0 ? customMessage : '', true);
+        const inBase64 = await imageService.trasnformImageTo64(path);
+        const response = new MessageMedia();
+        response.mimetype = "image/png";
+        response.data = inBase64;
+        response.filesize = size;
+        await client.sendMessage(sender, response, { sendMediaAsSticker: true });
     } catch (e) {
-        msg.reply("❌ Não foi possível gerar um sticker com essa mídia.")
+        msg.reply("❌ Não foi possível gerar um sticker com essa mídia.");
     }
-}
+};
 
 
 const generateSticker = async (msg, sender) => {
     if (msg.hasMedia) {
         try {
-            const customMessage = msg.body.split('.sticker')[1]
-            const { pathResponse } = await imageService.downloadImage(msg)
-            const { path, size } = await imageService.imageManipulation(pathResponse, customMessage.length > 0 ? customMessage : '', false)
-            const inBase64 = await imageService.trasnformImageTo64(path)
-            const response = new MessageMedia()
-            response.mimetype = "image/png"
-            response.data = inBase64
-            response.filesize = size
-            await client.sendMessage(sender, response, { sendMediaAsSticker: true })
+            const customMessage = msg.body.split('.sticker')[1];
+            const { pathResponse } = await imageService.downloadImage(msg);
+            const { path, size } = await imageService.imageManipulation(pathResponse, customMessage.length > 0 ? customMessage : '', false);
+            const inBase64 = await imageService.trasnformImageTo64(path);
+            const response = new MessageMedia();
+            response.mimetype = "image/png";
+            response.data = inBase64;
+            response.filesize = size;
+            await client.sendMessage(sender, response, { sendMediaAsSticker: true });
         } catch (e) {
-            msg.reply("❌ Não foi possível gerar um sticker com essa mídia.")
+            msg.reply("❌ Não foi possível gerar um sticker com essa mídia.");
         }
     } else {
         try {
-            const url = msg.body.substring(msg.body.indexOf(" ")).trim()
-            const data = await MessageMedia.fromUrl(url)
-            await client.sendMessage(sender, data, { sendMediaAsSticker: true })
+            const url = msg.body.substring(msg.body.indexOf(" ")).trim();
+            const data = await MessageMedia.fromUrl(url);
+            await client.sendMessage(sender, data, { sendMediaAsSticker: true });
         } catch (e) {
-            msg.reply("❌ Não foi possível gerar um sticker com esse link.")
+            msg.reply("❌ Não foi possível gerar um sticker com esse link.");
         }
     }
-}
+};
 
 const eightBallMsg = async (msg) => {
     let randomNumber = Math.floor(Math.random() * 8);
@@ -129,12 +136,12 @@ const eightBallMsg = async (msg) => {
             eightBallMsg = "Você está sem sorte."; // NÃO
             break;
     }
-    msg.reply(`A bola 8 mágica diz... ${eightBallMsg}`)
-}
+    msg.reply(`A bola 8 mágica diz... ${eightBallMsg}`);
+};
 
 const rollDice = async (msg) => {
-    let numFaceDice = msg.body.substring(msg.body.indexOf(" ")).trim()
-    if (!Number.isInteger(parseInt(numFaceDice))) numFaceDice = 6
+    let numFaceDice = msg.body.substring(msg.body.indexOf(" ")).trim();
+    if (!Number.isInteger(parseInt(numFaceDice))) numFaceDice = 6;
     let rollResult = Math.ceil(Math.random() * parseInt(numFaceDice));
-    msg.reply(`🎲 ${rollResult} rolado em um D${parseInt(numFaceDice)}`)
-}
+    msg.reply(`🎲 ${rollResult} rolado em um D${parseInt(numFaceDice)}`);
+};
